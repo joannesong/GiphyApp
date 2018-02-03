5ed9e72b2a578639141c2d267a8e8770d1c2bcf3
Joanne Song <joannesong@ac.c4q.nyc>
2018-02-03 13:00:45 -0500

    added networking and UI for MainActivity

---
+++ app/src/main/java/nyc/c4q/tariquapart2/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package nyc.c4q.tariquapart2;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by joannesong on 2/1/18.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/nyc/c4q/tariquapart2/network/Data.java
@@ -0,0 +1,15 @@
+package nyc.c4q.tariquapart2.network;
+
+import java.util.List;
+
+/**
+ * Created by joannesong on 2/3/18.
+ */
+
+public class Data {
+    List<ImageGiphy> data;
+
+    public List<ImageGiphy> getData() {
+        return data;
+    }
+}
