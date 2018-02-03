a0e6ca4360f83884a6e4f925bd6e46c5cca82c2f
T.Nehisi <tariquanehisi@ac.c4q.nyc>
2018-02-02 23:29:45 -0500

    +created the SQLDatabaseHelper for the GIPHY api +updated build.gradle with Cupboard dependency

---
+++ app/src/main/java/nyc/c4q/tariquapart2/Doggie.java
@@ -0,0 +1,9 @@
+package nyc.c4q.tariquapart2;
+
+/**
+ * Created by MsNehisi on 2/2/18.
+ */
+
+public class Doggie {
+//    POJO created from DTO from API - whatever you have this model named as in your project - pls update Line 31 in GiphyDatabaseHelper
+}
